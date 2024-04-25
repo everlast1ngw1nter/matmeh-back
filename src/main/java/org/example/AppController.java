30d62e2545ecc4f1701d7a454dbc27d47c03f519
everlast1ngw1nter <at07012004@gmail.com>
2024-04-25 18:57:35 +0500

    readme and ports fixed

--- src/main/java/org/example/AppController.java
+++ src/main/java/org/example/AppController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:3000", "http://localhost:5000", "http://localhost:63342"})
 public class AppController {
 
     public AppController(@Autowired DbService dbService) {
