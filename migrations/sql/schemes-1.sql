320757a54dbe0bc919049549be012f88d88f4bc9
everlast1ngw1nter <at07012004@gmail.com>
2024-05-27 19:08:41 +0500

    docker update

--- migrations/sql/schemes-1.sql
+++ migrations/sql/schemes-1.sql
@@ -2,5 +2,9 @@ CREATE TABLE schedule(
                          pair_number INT,
                          auditory VARCHAR(6),
                          date TIMESTAMP,
+                         group_name TEXT,
+                         lesson TEXT,
+                         lesson_type TEXT,
+                         teacher TEXT,
                          PRIMARY KEY (pair_number, auditory, date)
 );
